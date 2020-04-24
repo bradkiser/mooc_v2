49ad50b7be303109ce08efb7cddc5bb45e9bb903
bkiser <bkiser@republicwireless.com>
2020-04-24 14:02:15 -0400

    weekFive exercise089

--- src/main/java/weekFive/Exercise089/Clock.java
+++ src/main/java/weekFive/Exercise089/Clock.java
@@ -1,4 +1,30 @@
 package weekFive.Exercise089;
 
+import weekFour.Exercise078.BoundedCounter;
+
 public class Clock {
+    private BoundedCounter hours;
+    private BoundedCounter minutes;
+    private BoundedCounter seconds;
+
+    public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
+        this.hours = new BoundedCounter(hoursAtBeginning, 23);
+        this.minutes = new BoundedCounter(minutesAtBeginning, 59);
+        this.seconds = new BoundedCounter(secondsAtBeginning, 59);
+    }
+
+    public void tick() {
+        this.seconds.next();
+        if (this.seconds.getValue() == 0) {
+            this.minutes.next();
+
+            if (this.minutes.getValue() == 0) {
+                this.hours.next();
+            }
+        }
+    }
+
+    public String toString() {
+        return this.hours + ":" + this.minutes + ":" + this.seconds;
+    }
 }
