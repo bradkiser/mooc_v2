0f198621bd5f001edeb5edf41517e96ab3221b7a
bkiser <bkiser@republicwireless.com>
2020-04-25 10:27:44 -0400

    weekFive exercise 091-093

--- src/main/java/weekFive/Exercise093/Person.java
+++ src/main/java/weekFive/Exercise093/Person.java
@@ -1,4 +1,51 @@
 package weekFive.Exercise093;
 
+import java.util.Calendar;
+
+import weekFive.Exercise091.MyDate;
+
 public class Person {
+    private String name;
+    private MyDate birthday;
+
+    public Person(String name, int pp, int kk, int vv) {
+        this.name = name;
+        this.birthday = new MyDate(pp, kk, vv);
+    }
+
+    public Person(String name, MyDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public Person(String name) {
+        this.name = name;
+        this.birthday = getCurrentDate();
+    }
+
+    public int age() {
+        return this.birthday.differenceInYears(getCurrentDate());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
+        return this.name + ", born " + this.birthday;
+    }
+
+    public boolean olderThan(Person compared) {
+        return this.birthday.earlier(compared.birthday);
+    }
+
+    public MyDate getCurrentDate() {
+        int day = Calendar.getInstance().get(Calendar.DATE);
+        int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        MyDate currentDate = new MyDate(day, month, year);
+
+        return currentDate;
+    }
 }
